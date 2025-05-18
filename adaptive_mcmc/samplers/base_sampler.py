(major)(refactor) fix gradient flow, adaptive loss bug (accept prob), add fast matvec and tridiag prec

adaptive_mcmc/samplers/base_sampler.py
@@ -14,26 +14,30 @@ from adaptive_mcmc.distributions.distribution import Distribution
 
 
 @dataclass
-class Params(ABC):
+class CommonParams(ABC):
     target_dist: Optional[Union[torchDist, Distribution]] = None
     starting_point: Optional[Tensor] = None
     proposal_dist: Optional[Union[torchDist, Distribution]] = None
-    device: str = "cpu"
 
-    def copy(self) -> "Params":
+    def copy(self) -> "CommonParams":
         return deepcopy(self)
 
-    def update(self, new_params: "Params") -> None:
+    def update(self, new_params: "CommonParams") -> None:
         for field_name in new_params.__dict__:
             new_value = getattr(new_params, field_name)
             if new_value is not None:
                 setattr(self, field_name, new_value)
 
-    def copy_update(self, new_params: "Params") -> None:
+    def copy_update(self, new_params: "CommonParams") -> None:
         updated = self.copy()
         updated.update(new_params)
 
         return updated
+
+
+@dataclass
+class FixedParams(ABC):
+    device: str = "cpu"
 
 
 @dataclass
@@ -50,7 +54,7 @@ class Cache:
     accept_prob_hist: list[float] = field(default_factory=list[float])
 
 
-def update_params(params: Params, cache: Cache, new_params: Params) -> None:
+def update_params(params: CommonParams, cache: Cache, new_params: CommonParams) -> None:
     if new_params is not None:
         params.update(new_params)
 
@@ -61,18 +65,20 @@ def update_params(params: Params, cache: Cache, new_params: Params) -> None:
 @dataclass
 class Iteration(ABC):
     cache: Cache = field(default_factory=Cache)
-    params: Params = field(default_factory=Params)
+    common_params: CommonParams = field(default_factory=CommonParams)
+    fixed_params: FixedParams = field(default_factory=FixedParams)
     collect_required: bool = False
     index: int = 0
 
     def init(self, cache=None) -> None:
-        self.cache.point = self.params.starting_point.clone().requires_grad_()
-        self.cache.logp = self.params.target_dist.log_prob(self.cache.point)
+        self.cache.point = self.common_params.starting_point.clone().requires_grad_()
+        self.cache.logp = self.common_params.target_dist.log_prob(self.cache.point)
         self.cache.grad = torch.autograd.grad(
             self.cache.logp.sum(),
             self.cache.point,
-            retain_graph=True,
-        )[0].detach().requires_grad_()
+            retain_graph=False,
+        )[0].detach()
+        self.cache.point = self.cache.point.detach()
 
     @abstractmethod
     def run(self) -> Cache:
@@ -88,7 +94,7 @@ class Iteration(ABC):
 class MHIteration(Iteration):
     def MHStep(self, point_new: Tensor, logp_new: Tensor, grad_new: Tensor, accept_prob: Tensor) -> None:
         with torch.no_grad():
-            mask = torch.rand_like(accept_prob, device=self.params.device) < accept_prob
+            mask = torch.rand_like(accept_prob, device=self.fixed_params.device) < accept_prob
 
             self.cache.point[mask] = point_new[mask]
             self.cache.logp[mask] = logp_new[mask]
@@ -96,9 +102,9 @@ class MHIteration(Iteration):
 
             self.cache.accept_prob_hist.append(accept_prob.mean().item())
 
-        self.cache.point = self.cache.point.detach().requires_grad_(self.cache.point.requires_grad)
-        self.cache.logp = self.cache.logp.detach().requires_grad_(self.cache.logp.requires_grad)
-        self.cache.grad = self.cache.grad.detach().requires_grad_(self.cache.grad.requires_grad)
+        self.cache.point = self.cache.point.detach()    # .requires_grad_(self.cache.point.requires_grad)
+        self.cache.logp = self.cache.logp.detach()  # .requires_grad_(self.cache.logp.requires_grad)
+        self.cache.grad = self.cache.grad.detach()  # .requires_grad_(self.cache.grad.requires_grad)
 
 
 def track_runtime(func):
@@ -128,8 +134,9 @@ class SampleBlock:
     pure_runtime: float = 0.
     callback: Optional[Callable] = None
 
-    def run(self, cache: Optional[Cache] = None, params: Optional[Params] = None, progress: bool = True) -> Tuple[Cache, Params]:
-        update_params(self.iteration.params, cache, params)
+    def run(self, cache: Optional[Cache] = None, common_params: Optional[CommonParams] = None,
+            progress: bool = True) -> Tuple[Cache, CommonParams]:
+        update_params(self.iteration.common_params, cache, common_params)
 
         if cache is not None:
             if isinstance(cache, type(self.iteration.cache)):
@@ -155,7 +162,7 @@ class SampleBlock:
                 if stop_status.is_stop:
                     break
 
-        return self.iteration.cache, self.iteration.params
+        return self.iteration.cache, self.iteration.common_params
 
 
 @dataclass
@@ -168,13 +175,13 @@ class Pipeline:
         self.sample_blocks.append(block)
 
     @track_runtime
-    def run(self, cache: Optional[Cache] = None, params: Optional[Params] = None) -> None:
+    def run(self, cache: Optional[Cache] = None, params: Optional[CommonParams] = None) -> None:
         print("number of blocks:", len(self.sample_blocks))
 
         sample_count = 0
         for block in self.sample_blocks:
-            shape = block.iteration.params.starting_point.shape
-            device = block.iteration.params.device
+            shape = block.iteration.common_params.starting_point.shape
+            device = block.iteration.fixed_params.device
             if block.iteration.collect_required:
                 block.iteration.index = sample_count
                 sample_count += block.iteration_count
@@ -201,7 +208,7 @@ class Algorithm(ABC):
     name: str
 
     @abstractmethod
-    def load_params(self, params: Params):
+    def load_params(self, common_params: CommonParams):
         pass
 
     def run(self):

adaptive_mcmc/samplers/hmc.py
@@ -1,5 +1,6 @@
 from contextlib import nullcontext
 from dataclasses import dataclass, field
+from enum import Enum
 from typing import Callable, Dict, List, Optional, Union
 
 import torch
@@ -8,57 +9,56 @@ from torch.distributions import Distribution as torchDist
 
 from adaptive_mcmc.distributions.distribution import Distribution
 from adaptive_mcmc.samplers import base_sampler
+from adaptive_mcmc.linalg import fastmv
+
+
+class PrecType(str, Enum):
+    Dense = "dense"
+    TRIDIAG = "tridiag"
+    NONE = "none"
 
 
 @dataclass
-class HMCParams(base_sampler.Params):
+class HMCCommonParams(base_sampler.CommonParams):
     prec: Optional[Tensor] = None
     Minv: Optional[Tensor] = None
     lf_step_size: Union[float, Tensor] = 1e-2
     lf_step_size_adaptive_rate: float = 1e-2
     lf_step_count: int = 5
+
+
+@dataclass
+class HMCFixedParams(base_sampler.FixedParams):
     target_acceptance: float = 0.65
     stop_grad: bool = False
     no_grad: bool = True
+    prec_type: PrecType = PrecType.NONE
 
 
 class Leapfrog():
     def step(self, q_prev: Tensor, p_prev: Tensor, gradq_prev: Tensor,
-             Minv: Tensor, target_dist: Union[Distribution, torchDist],
+             matvec_minv: Callable, target_dist: Union[Distribution, torchDist],
              step_size: float = 1e-3, stop_grad: bool = True, no_grad: bool = False) -> dict:
 
         context = torch.no_grad() if no_grad else nullcontext()
 
         with context:
             p_half = p_prev + 0.5 * step_size * gradq_prev
-            q_next = q_prev + step_size * torch.einsum("...ij,...j->...i", Minv, p_half)
+            q_next = q_prev + step_size * matvec_minv(p_half)
+            # torch.einsum("...ij,...j->...i", Minv, p_half)
 
-        if stop_grad:
-            q_next = q_next.detach().requires_grad_(True)
-        else:
-            q_next = q_next.requires_grad_(True)
+        q_next = q_next.requires_grad_(True)
 
         logq_next = target_dist.log_prob(q_next)
         gradq_next = torch.autograd.grad(logq_next.sum(), q_next, retain_graph=not no_grad)[0]
 
-        if stop_grad:
-            gradq_next = gradq_next.detach().requires_grad_(not no_grad)
+        if no_grad:
+            q_next = q_next.requires_grad_(False)
+            gradq_next = gradq_next.detach()
+            q_next = q_next.detach()
 
         with context:
             p_next = p_half + 0.5 * step_size * gradq_next
-
-        # nan_counts = {
-        #     "q_prev": torch.isnan(q_prev).sum().item(),
-        #     "p_prev": torch.isnan(p_prev).sum().item(),
-        #     "gradq_prev": torch.isnan(gradq_prev).sum().item(),
-        #     "Minv": torch.isnan(Minv).sum().item(),
-        #     'p_half': torch.isnan(p_half).sum().item(),
-        #     'q_next': torch.isnan(q_next).sum().item(),
-        #     'logq_next': torch.isnan(logq_next).sum().item(),
-        #     'gradq_next': torch.isnan(gradq_next).sum().item(),
-        #     'p_next': torch.isnan(p_next).sum().item(),
-        # }
-        # print(f"[Leapfrog.step] NaN counts: {nan_counts}")
 
         return {
             "q": q_next,
@@ -66,7 +66,7 @@ class Leapfrog():
             "gradq": gradq_next,
         }
 
-    def run(self, q: Tensor, p: Tensor, gradq: Tensor, Minv: Tensor,
+    def run(self, q: Tensor, p: Tensor, gradq: Tensor, matvec_minv: Callable,
             target_dist: Union[Distribution, torchDist],
             step_count: int = 5, step_size: float = 1e-3,
             stop_grad: bool = True, no_grad: bool = False, keep_trajectory=False) -> Optional[List[dict]]:
@@ -78,7 +78,7 @@ class Leapfrog():
             try:
                 ret = self.step(
                     q_prev=ret["q"], p_prev=ret["p"], gradq_prev=ret["gradq"],
-                    Minv=Minv, target_dist=target_dist, step_size=step_size,
+                    matvec_minv=matvec_minv, target_dist=target_dist, step_size=step_size,
                     stop_grad=stop_grad, no_grad=no_grad,
                 )
 
@@ -92,7 +92,7 @@ class Leapfrog():
 
             # happens when NaNs are in ret self.step computation
             except ValueError:
-                print("val err")
+                # print("val err")
                 return None, None
 
             trajectory.append(ret)
@@ -102,7 +102,8 @@ class Leapfrog():
 
 @dataclass
 class HMCIter(base_sampler.MHIteration):
-    params: HMCParams = field(default_factory=HMCParams)
+    common_params: HMCCommonParams = field(default_factory=HMCCommonParams)
+    fixed_params: HMCFixedParams = field(default_factory=HMCFixedParams)
     lf_intergrator: Leapfrog = field(default_factory=Leapfrog)
     adapt_step_size_required: bool = False
 
@@ -111,77 +112,122 @@ class HMCIter(base_sampler.MHIteration):
         self.step_id = 0
 
         if hasattr(self.cache, "prec") and self.cache.prec is not None:
-            self.params.prec = self.cache.prec
-            self.params.Minv = torch.einsum("...ij,...kj->...ik", self.cache.prec, self.cache.prec)
-        elif self.params.prec is None:
-            self.params.prec = torch.eye(self.cache.point.shape[-1],
-                                         device=self.params.device).repeat(*self.cache.point.shape[:-1], 1, 1)
-            self.params.Minv = self.params.prec
+            pass
+        elif self.common_params.prec is None:
+            self.cache.prec = torch.eye(
+                self.cache.point.shape[-1],
+                device=self.fixed_params.device
+            ).repeat(*self.cache.point.shape[:-1], 1, 1)
+        else:
+            self.cache.prec = self.common_params.prec
 
-        if isinstance(self.params.lf_step_size, float):
-            self.params.lf_step_size = torch.full((self.cache.point.shape[0], 1), self.params.lf_step_size)
+        self.make_matvecs(self.cache.prec)
 
-    def _eval_trajectory(self, noise, prec, Minv):
+        if isinstance(self.common_params.lf_step_size, float):
+            self.common_params.lf_step_size = torch.full(
+                (self.cache.point.shape[0], 1),
+                self.common_params.lf_step_size,
+            )
+
+    def make_matvecs(self, prec: Tensor, dimension=None):
+        if self.fixed_params.prec_type == PrecType.TRIDIAG:
+            diag = torch.exp(prec[:, :dimension])
+            lower = prec[:, dimension:]
+
+        def matvec_minv(v: Tensor) -> Tensor:
+            return matvec_c(matvec_ct(v))
+
+        def matvec_ct(v: Tensor) -> Tensor:
+            if self.fixed_params.prec_type == PrecType.NONE:
+                return v
+            elif self.fixed_params.prec_type == PrecType.TRIDIAG:
+                return fastmv.tridiag_matmul(v, diag=diag, upper=lower)
+            else:
+                return torch.einsum("...ji,...j->...i", prec, v)
+
+        def matvec_c(v: Tensor) -> Tensor:
+            if self.fixed_params.prec_type == PrecType.NONE:
+                return v
+            elif self.fixed_params.prec_type == PrecType.TRIDIAG:
+                return fastmv.tridiag_matmul(v, diag=diag, lower=lower)
+            else:
+                return torch.einsum("...ij,...j->...i", prec, v)
+
+        self.matvec_minv = matvec_minv
+        self.matvec_ct = matvec_ct
+        self.matvec_c = matvec_c
+
+    def _eval_trajectory(self, noise: Tensor, prec: Tensor, context):
         trajectory = None
-        p = torch.linalg.solve_triangular(
-            torch.einsum("...ij->...ji", prec),
-            noise.unsqueeze(-1),
-            upper=True,
-        ).squeeze(-1)
+        d = noise.shape[-1]
+
+        with context:
+            if self.fixed_params.prec_type == PrecType.NONE:
+                p = noise
+            elif self.fixed_params.prec_type == PrecType.TRIDIAG:
+                p = fastmv.bidiag_solve_jit(noise, torch.exp(prec[:, :d]), prec[:, d:])
+            else:
+                p = torch.linalg.solve_triangular(
+                    torch.einsum("...ij->...ji", prec),
+                    noise.unsqueeze(-1),
+                    upper=True,
+                ).squeeze(-1)
 
         while trajectory is None:
             trajectory, mask = self.lf_intergrator.run(
                 q=self.cache.point,
                 p=p,
                 gradq=self.cache.grad,
-                Minv=Minv,
-                target_dist=self.params.target_dist,
-                step_count=self.params.lf_step_count,
-                step_size=self.params.lf_step_size,
-                stop_grad=self.params.stop_grad,
-                no_grad=self.params.no_grad,
+                matvec_minv=self.matvec_minv,
+                target_dist=self.common_params.target_dist,
+                step_count=self.common_params.lf_step_count,
+                step_size=self.common_params.lf_step_size,
+                stop_grad=self.fixed_params.stop_grad,
+                no_grad=self.fixed_params.no_grad,
             )
 
             if trajectory is None:
-                print("bad traj", mask)
+                # print("bad traj", mask)
                 with torch.no_grad():
-                    self.params.lf_step_size = self.params.lf_step_size * (
-                        1 - self.params.lf_step_size_adaptive_rate * mask.unsqueeze(-1)
+                    self.common_params.lf_step_size = self.common_params.lf_step_size * (
+                        1 - self.common_params.lf_step_size_adaptive_rate * (1 if mask is None else mask.unsqueeze(-1))
                     )
 
         return trajectory[-1]["q"], trajectory
 
-    def _run_iter(self, prec: Tensor, Minv: Tensor, noise_grad: bool = False) -> Dict[str, Tensor]:
-        context = torch.no_grad() if self.params.no_grad else nullcontext()
+    def _run_iter(self, prec: Tensor, noise_grad: bool = False) -> Dict[str, Tensor]:
+        context = torch.no_grad() if self.fixed_params.no_grad else nullcontext()
 
-        noise = self.params.proposal_dist.sample(self.cache.point.shape[:-1]).requires_grad_(noise_grad)
-        _, trajectory = self._eval_trajectory(noise, prec, Minv)
+        noise = self.common_params.proposal_dist.sample(self.cache.point.shape[:-1]).requires_grad_(noise_grad)
 
-        if noise_grad:
-            trajectory[0]["noise"] = noise
+        _, trajectory = self._eval_trajectory(noise, prec, context)
 
         with context:
+            trajectory[0]["noise"] = noise
+
             p = trajectory[0]["p"]
 
             point_new = trajectory[-1]["q"]
-            logp_new = self.params.target_dist.log_prob(point_new)
+            logp_new = self.common_params.target_dist.log_prob(point_new)
             grad_new = trajectory[-1]["gradq"]
             p_new = trajectory[-1]["p"]
 
+            new_norm = self.matvec_ct(p_new).pow(2).sum(dim=-1)
+            old_norm = self.matvec_ct(p).pow(2).sum(dim=-1)
+
             energy_error = (
                 -logp_new + self.cache.logp.detach()
-                + 0.5 * (
-                    torch.einsum("...i,...ij,...j->...", p_new, Minv, p_new)
-                    - torch.einsum("...i,...ij,...j->...", p, Minv, p)
-                )
+                + 0.5 * (new_norm - old_norm)
             )
 
             accept_prob = torch.exp(torch.clamp(-energy_error, max=0))
 
             if self.adapt_step_size_required:
                 with torch.no_grad():
-                    self.params.lf_step_size = self.params.lf_step_size * (
-                        1 + self.params.lf_step_size_adaptive_rate * (accept_prob.unsqueeze(-1) - self.params.target_acceptance)
+                    self.common_params.lf_step_size = self.common_params.lf_step_size * (
+                        1 + self.common_params.lf_step_size_adaptive_rate * (
+                            accept_prob.unsqueeze(-1) - self.fixed_params.target_acceptance
+                        )
                     )
 
             self.MHStep(
@@ -200,34 +246,46 @@ class HMCIter(base_sampler.MHIteration):
             "energy_error": energy_error,
             "accept_prob": accept_prob,
             "trajectory": trajectory,
-            "grad_new": grad_new,
         }
 
     def run(self):
-        self._run_iter(prec=self.params.prec, Minv=self.params.Minv)
+        self._run_iter(prec=self.cache.prec)
 
 
 @dataclass
 class HMCVanilla(base_sampler.AlgorithmStoppingRule):
-    params: HMCParams
     step_size_burn_in_iter_count: int
     burn_in_iter_count: int
     sample_iter_count: int
     probe_period: int
     stopping_rule: Callable
+    common_params: HMCCommonParams = field(default_factory=HMCCommonParams)
+    fixed_params: HMCFixedParams = field(default_factory=HMCFixedParams)
 
-    def load_params(self, params: base_sampler.Params):
+    def load_params(
+        self,
+        common_params: base_sampler.CommonParams,
+    ):
         self.pipeline = base_sampler.Pipeline([
             base_sampler.SampleBlock(
-                iteration=HMCIter(params=params, adapt_step_size_required=True),
+                iteration=HMCIter(
+                    common_params=common_params,
+                    fixed_params=self.fixed_params,
+                    adapt_step_size_required=True
+                ),
                 iteration_count=self.burn_in_iter_count,
-                callback=lambda: print(f"step_size={params.lf_step_size.mean()}")
+                callback=lambda: print(f"step_size={common_params.lf_step_size.mean()}")
             ),
             base_sampler.SampleBlock(
-                iteration=HMCIter(params=params, adapt_step_size_required=True, collect_required=True),
+                iteration=HMCIter(
+                    common_params=common_params,
+                    fixed_params=self.fixed_params,
+                    adapt_step_size_required=True,
+                    collect_required=True
+                ),
                 iteration_count=self.sample_iter_count,
                 stopping_rule=self.stopping_rule,
                 probe_period=self.probe_period,
-                callback=lambda: print(f"step_size={params.lf_step_size.mean()}")
+                callback=lambda: print(f"step_size={common_params.lf_step_size.mean()}")
             ),
         ])