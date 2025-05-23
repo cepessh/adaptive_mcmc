b2922d2aa29607cf7dcfd25f8be4d707b9aef73d
Kamil Mardanshin <mardanshin.kamil@gmail.com>
2025-05-23 17:55:40 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adaptive_mcmc",
+    version="0.1",
+    packages=find_packages(),   # finds adaptive_mcmc and sub‑packages
+    install_requires=[],       # put runtime deps here, e.g. ["torch>=1.12"]
+)
